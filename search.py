48e335681e3a255430b1c07ac4e9a233aa92282e
AliTekol <alitekol65@gmail.com>
2019-07-31 17:08:56 +0300

    updated

--- search.py
+++ search.py
@@ -2,14 +2,13 @@ import requests
 import json
 
 def github_search(a,b):
-    files = []
+    links = []
     params = {'q': a +'language:'+ b}
     response = requests.get('https://api.github.com/search/repositories', params=params)
     json_file = response.json()
     for item in json_file['items']:
-        files.append(item['url'])
-        
-    return files
+        links.append(item['url'])      
+    return links
 
 query = "arduino"
 language = "python"
--- search_test.py
+++ search_test.py
@@ -2,14 +2,10 @@ import unittest
 from search import github_search
 
 class SearchTest(unittest.TestCase): 
-    def test(self):
-        
+    def test(self):    
         output = github_search("arduino", "python")
-        num_lines = 0  
-        for line in output:
-            num_lines += 1
-        print("Count of the sites :",num_lines)
-        self.assertGreaterEqual(num_lines,1)
+        count = len(output)
+        self.assertGreaterEqual(count,1)
 
 if __name__ == '__main__':
     unittest.main()
